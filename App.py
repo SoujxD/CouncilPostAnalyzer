64198ca6089d8750fcef00c16c7af427628ed368
SoujxD <117734372+SoujxD@users.noreply.github.com>
2023-05-04 10:43:50 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,344 @@
+import streamlit as st
+import pandas as pd
+import base64,random
+import time,datetime
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io,random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+from Courses import cs_course,sports_course,literature_course,publicity_course,general_course
+import pafy
+import plotly.express as px
+
+def fetch_yt_video(link):
+    video = pafy.new(link)
+    return video.title
+
+def get_table_download_link(df,filename,text):
+ 
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+def course_recommender(course_list):
+    st.subheader("**Courses & Certificates🎓 Recommendations**")
+    c = 0
+    rec_course = []
+    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 4)
+    random.shuffle(course_list)
+    for c_name, c_link in course_list:
+        c += 1
+        st.markdown(f"({c}) [{c_name}]({c_link})")
+        rec_course.append(c_name)
+        if c == no_of_reco:
+            break
+    return rec_course
+
+connection = pymysql.connect(host='localhost',user='root',password='',db='sra')
+cursor = connection.cursor()
+
+def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
+    DB_table_name = 'user_data'
+    insert_sql = "insert into " + DB_table_name + """
+    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+st.set_page_config(
+   page_title="Council Post Analyser"
+
+)
+def run():
+    st.title("Council Post Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    # link = '[©Developed by Spidy20](http://github.com/spidy20)'
+    # st.sidebar.markdown(link, unsafe_allow_html=True)
+
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS SRA;"""
+    cursor.execute(db_sql)
+
+    # Create table
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                     Name varchar(100) NOT NULL,
+                     Email_ID VARCHAR(50) NOT NULL,
+                     resume_score VARCHAR(8) NOT NULL,
+                     Timestamp VARCHAR(50) NOT NULL,
+                     Page_no VARCHAR(5) NOT NULL,
+                     Predicted_Field VARCHAR(25) NOT NULL,
+                     User_level VARCHAR(30) NOT NULL,
+                     Actual_skills VARCHAR(300) NOT NULL,
+                     Recommended_skills VARCHAR(300) NOT NULL,
+                     Recommended_courses VARCHAR(600) NOT NULL,
+                     PRIMARY KEY (ID));
+                    """
+    cursor.execute(table_sql)
+    if choice == 'Normal User':
+        # st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Upload your resume, and get smart recommendation based on it."</h4>''',
+        #             unsafe_allow_html=True)
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            # with st.spinner('Uploading your Resume....'):
+            #     time.sleep(4)
+            save_image_path = './Resumes/'+pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                ## Get the whole resume data
+                resume_text = pdf_reader(save_image_path)
+
+                st.header("**Resume Analysis**")
+                st.success("Hello "+ resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: '+resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: '+str(resume_data['no_of_pages']))
+                except:
+                    pass
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown( '''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >=3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',unsafe_allow_html=True)
+
+                st.subheader("**Skills Recommendation💡**")
+                ## Skill shows
+                keywords = st_tags(label='### Skills that you have',
+                text='See our skills recommendation',
+                    value=resume_data['skills'],key = '1')
+
+                ##  recommendation
+                cultural_secretory = ['multitasking' , 'organised','team ethic', 'communication skills','business analytics','goal-oriented','professional guide']
+                sports_secretory = ['athletic','bold','competitor','player','time management','orator','leadership skill' ,'detail oriented']
+                literature_head = ['communication skills','orator','language arts','discipline','strong vocabulary','writing skills','open to changes','content writing','relevance']
+                publicity_head = ['speech','marketing','team work','enthusiasm','communication skills','digital marketing','content development','brand building']
+                general_secretory = ['orator','team building','adaptibility','affable','note instigator','guide']
+
+                recommended_skills = []
+                reco_field = ''
+                rec_course = ''
+                ## Courses recommendation
+                for i in resume_data['skills']:
+                    ## Data science recommendation
+                    if i.lower() in cultural_secretory:
+                        print(i.lower())
+                        reco_field = 'Cultural Secretory'
+                        st.success("** Our analysis says you are Eligible for Cultural Secretory Post.**")
+                        recommended_skills = ['multitasking' , 'organised','team ethic', 'communication skills','business analytics','goal-oriented','professional guide']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '2')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Post💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(cs_course)
+                        break
+
+                    ## Web development recommendation
+                    elif i.lower() in sports_secretory:
+                        print(i.lower())
+                        reco_field = 'Sports Secretory'
+                        st.success("** Our analysis says you are Eligible for Sports Secretory Post.**")
+                        recommended_skills = ['athletic','bold','competitor','player','time management','orator','leadership skill' ,'detail oriented']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '3')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Post💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(sports_course)
+                        break
+
+                    ## Android App Development
+                    elif i.lower() in literature_head:
+                        print(i.lower())
+                        reco_field = 'Literature Head'
+                        st.success("** Our analysis says you are Eligible for Literature Head Post. **")
+                        recommended_skills = ['communication skills','orator','language arts','discipline','strong vocabulary','writing skills','open to changes','content writing','relevance']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '4')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a post💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(literature_course)
+                        break
+
+                    ## IOS App Development
+                    elif i.lower() in publicity_head:
+                        print(i.lower())
+                        reco_field = 'Publicity Head'
+                        st.success("** Our analysis says you are Eligible for Publicity Head Post **")
+                        recommended_skills = ['speech','marketing','team work','enthusiasm','communication skills','digital marketing','content development','brand building']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '5')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Post💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(publicity_course)
+                        break
+
+                    ## Ui-UX Recommendation
+                    elif i.lower() in general_secretory:
+                        print(i.lower())
+                        reco_field = 'General Secretory'
+                        st.success("** Our analysis says you are Eligible for General Secretory Post **")
+                        recommended_skills = ['orator','team building','adaptibility','affable','note instigator','guide']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '6')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a post💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(general_course)
+                        break
+
+                #
+                ## Insert into table
+                ts = time.time()
+                cur_date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                cur_time = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                timestamp = str(cur_date+'_'+cur_time)
+
+                ### Resume writing recommendation
+                st.subheader("**Resume Tips & Ideas💡**")
+                resume_score = 0
+                if 'Objective' in resume_text:
+                    resume_score = resume_score+20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Objective</h4>''',unsafe_allow_html=True)
+                else:
+                    st.markdown('''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add your career objective, it will give your career intension to the Recruiters.</h4>''',unsafe_allow_html=True)
+
+                if 'Ambition'  in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Delcaration✍/h4>''',unsafe_allow_html=True)
+                else:
+                    st.markdown('''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Declaration✍. It will give the assurance that everything written on your resume is true and fully acknowledged by you</h4>''',unsafe_allow_html=True)
+
+                if 'Hobbies' or 'Interests'in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Hobbies⚽</h4>''',unsafe_allow_html=True)
+                else:
+                    st.markdown('''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Hobbies⚽. It will show your persnality to the Recruiters and give the assurance that you are fit for this role or not.</h4>''',unsafe_allow_html=True)
+
+                if 'Achievements' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Achievements🏅 </h4>''',unsafe_allow_html=True)
+                else:
+                    st.markdown('''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Achievements🏅. It will show that you are capable for the required position.</h4>''',unsafe_allow_html=True)
+
+                if 'Projects' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Projects👨‍💻 </h4>''',unsafe_allow_html=True)
+                else:
+                    st.markdown('''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Projects👨‍💻. It will show that you have done work related the required position or not.</h4>''',unsafe_allow_html=True)
+
+                st.subheader("**Resume Score📝**")
+                st.markdown(
+                    """
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                score = 0
+                for percent_complete in range(resume_score):
+                    score +=1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(score)+'**')
+                st.warning("** Note: This score is calculated based on the content that you have added in your Resume. **")
+                st.balloons()
+
+                insert_data(resume_data['name'], resume_data['email'], str(resume_score), timestamp,
+                              str(resume_data['no_of_pages']), reco_field, cand_level, str(resume_data['skills']),
+                              str(recommended_skills), str(rec_course))
+
+
+                ## Resume writing video
+
+
+                connection.commit()
+            else:
+                st.error('Something went wrong..')
+    else:
+        ## Admin Side
+        st.success('Welcome to Admin Side')
+        # st.sidebar.subheader('**ID / Password Required!**')
+
+        ad_user = st.text_input("Username")
+        ad_password = st.text_input("Password", type='password')
+        if st.button('Login'):
+            if ad_user == 'Apsitcouncil' and ad_password == 'Apsit1234':
+                st.success("Welcome Soujanya")
+                # Display Data
+                cursor.execute('''SELECT*FROM user_data''')
+                data = cursor.fetchall()
+                st.header("**User's👨‍💻 Data**")
+                df = pd.DataFrame(data, columns=['ID', 'Name', 'Email', 'Resume Score', 'Timestamp', 'Total Page',
+                                                 'Predicted Field', 'User Level', 'Actual Skills', 'Recommended Skills',
+                                                 'Recommended Course'])
+                st.dataframe(df)
+                st.markdown(get_table_download_link(df,'User_Data.csv','Download Report'), unsafe_allow_html=True)
+                ## Admin Side Data
+                query = 'select * from user_data;'
+                plot_data = pd.read_sql(query, connection)
+
+                ## Pie chart for predicted field recommendations
+                labels = plot_data.Predicted_Field.unique()
+                print(labels)
+                values = plot_data.Predicted_Field.value_counts()
+                print(values)
+                st.subheader("📈 **Pie-Chart for Predicted Field Recommendations**")
+                fig = px.pie(df, values=values, names=labels, title='Predicted Field according to the Skills')
+                st.plotly_chart(fig)
+
+
+                ### Pie chart for User's👨‍💻 Experienced Level
+                labels = plot_data.User_level.unique()
+                values = plot_data.User_level.value_counts()
+                st.subheader("📈 ** Pie-Chart for User's👨‍💻 Experienced Level**")
+                fig = px.pie(df, values=values, names=labels, title="Pie-Chart📈 for User's👨‍💻 Experienced Level")
+                st.plotly_chart(fig)
+
+
+            else:
+                st.error("Wrong ID & Password Provided")
+run()
---
+++ Courses.py
@@ -0,0 +1,34 @@
+cs_course = [['Cultural Competency by UDEMY', 'https://www.udemy.com/share/103yzG/'],
+             ['Cross-Cultural Communication: How To Flex Your Style by UDEMY','https://www.udemy.com/share/101FA4/'],
+             ['Cross Cultural Leadership by UDEMY','https://www.udemy.com/share/104v02/'],
+             ['Cross Cultural Leadership by UDEMY','https://www.udemy.com/share/104v02/']]
+
+sports_course = [['Sports Management: The Essentials Course','https://www.udemy.com/share/103nrw/'],
+              ['Sports Agents and the Athlete Management Business','https://www.udemy.com/share/104JU8/'],
+              ['Sports Management: Data and Analytics','https://www.udemy.com/share/104Jw2/'],
+              ['Sports Management: Gameday Operations','https://www.udemy.com/share/104uf8/']]
+
+literature_course = [['English Literature: Be as Informed as a Literature Graduate','https://www.udemy.com/share/102E4W/'],
+                  ['Systematic Literature Review : A Practical Guide','https://www.udemy.com/share/101TP2/'],
+                  ['Head anatomy and sculpting exercises course','https://www.udemy.com/share/107E9e/'],
+                  ['World Literature: Your Guide to Becoming Very Well Read','https://www.udemy.com/share/103MwR/']]
+
+publicity_course = [['Press Coverage, Publicity & Public Relations For Branding','https://www.udemy.com/share/102v0G/'],
+              ['How to get featured on CNN, ABC, and NBC for free','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Get Publicity for Your Business as a Podcast Guest Expert','https://www.udemy.com/share/101qYI/'],
+              ['Get Local Publicity for Your Company, Yourself, Your Cause','https://www.udemy.com/share/102Wsu/']]
+
+general_course = [['SUPERHUMAN Productivity 10X: The Secret Skill Of Performance','https://www.udemy.com/course/productivity-10x-productivity/'],
+               ['Time Management, Prioritization and Productivity','https://www.udemy.com/course/time-management-prioritization-and-productivity/'],
+               ['20+ Productivity & Time Management Tips For Busy People','https://www.udemy.com/course/time-management-productivity-for-busy-professionals/'],
+               ['Time and Task Management: Time Management Techniques','https://www.udemy.com/course/time_task_management/']]
+
+resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
+                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
+                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
+                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
